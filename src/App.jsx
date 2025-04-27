9b094d905ac0a653a589ad1c6e72ad85c817ee2e
ByteMe6 <tsapiy.yurka@gmail.com>
2025-04-27 13:55:55 +0300

    02

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import React from 'react'
+import Greeting from './Greeting'
+import Message from './Message'
+import Button from './Button'
 import './App.css'
 
-function App() {
-  const name = "ByteMe6"
-  const favoriteSite = { siteName: "Google", url: "https://www.google.com/" }
-  const num1 = 5
-  const num2 = 10
-  const sum = num1 + num2
-  const colors = ["Червоний", "Синій", "Зелений"]
+const App = () => {
+  const handleClick = () => {
+    console.log('Кнопка натиснута!')
+  }
 
   return (
-    <div className="App">
-      <h1>{`Привіт, ${name}!`}</h1>
-      <p>Ласкаво просимо до нашого сайту!</p>
-      <img src="https://picsum.photos/200/300" alt="Приклад зображення" /> <br />
-      <a href={favoriteSite.url} target="_blank" rel="noopener noreferrer">
-        {favoriteSite.siteName}
-      </a>
-      <p>Сума: {sum}</p>
-      <ul>
-        {colors.map((color, index) => (
-          <li key={index}>{color}</li>
-        ))}
-      </ul>
+    <div>
+      <Greeting name="ByteMe6" />
+      <Message text="message (text)" />
+      <Button onClick={handleClick} />
     </div>
   )
 }
