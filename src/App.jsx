ef970af3cdb62c1f618e2ce0e8d1372257fa9df2
ByteMe6 <tsapiy.yurka@gmail.com>
2025-06-12 21:52:05 +0300

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,23 @@
 import { Component } from "react";
-import ContactForm from "./components/ContactForm";
-import ContactList from "./components/ContactList";
-import Filter from "./components/Filter";
+import Searchbar from "./components/Searchbar";
+import Images from "./components/Images";
 
 class App extends Component {
   state = {
-    contacts: [],
-    filter: ''
+    searchbarVal: '',
   };
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = (newContact) => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact]
-    }));
-  };
-
-  deleteContact = (contactId) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-    }));
-  };
-
-  handleFilterChange = (e) => {
-    this.setState({ filter: e.target.value });
-  };
-
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  handleSearch = (value) => {
+    console.log(value + ' - это значаение в файлике App.jsx');
+    this.setState({ searchbarVal: value });
   };
 
   render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-
     return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} contacts={this.state.contacts} />
-
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.handleFilterChange} />
-        <ContactList 
-          contacts={filteredContacts} 
-          onDelete={this.deleteContact} 
-        />
-      </div>
+      <main>
+        <Searchbar onSearch={this.handleSearch} />
+        <Images query={this.state.searchbarVal} />
+      </main>
     );
   }
 }
